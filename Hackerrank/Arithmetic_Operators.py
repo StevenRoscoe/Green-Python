7ca09bf104f83b96e65795505a4888c8bbb0fd4b
Steven Roscoe <dbzroscoe@gmail.com>
2022-10-31 18:34:45 +0000

    Hackerrank Challenges

--- Hackerrank/Arithmetic_Operators.py
+++ Hackerrank/Arithmetic_Operators.py
@@ -1,1 +1,8 @@
-
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    
+    #Function used to give the output of integers that are added, subtracted, and multiplied.
