152464e6c0fa651c04fee7d7421ff8394d06ff86
StevenRoscoe <112785884+StevenRoscoe@users.noreply.github.com>
2022-11-01 20:29:58 -0500

    Update Python_Division.py

--- Hackerrank/Python_Division.py
+++ Hackerrank/Python_Division.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
     a = int(input())
     b = int(input())
-    print(a//b) #Used to see how many times the integer can go into the other
-    print(a/b) #Used to divide the integer
+    print(a//b) #Divisibility with a remainder of _ 
+    print(a/b) #Dividing
     
