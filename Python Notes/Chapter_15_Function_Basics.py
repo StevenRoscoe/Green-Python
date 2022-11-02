1b592e2b11793a14950f0d8e7308e299723a40eb
Steven Roscoe <dbzroscoe@gmail.com>
2022-11-02 21:53:57 +0000

    In Progress

--- Python Notes/Chapter_15_Function_Basics.py
+++ Python Notes/Chapter_15_Function_Basics.py
@@ -5,13 +5,16 @@
 
 def print_name(name):
     print(f"Name is {name}")
-    
+                                    #Make sure to leave a space in-between the function to call the function
 print_name("Steven")
 
 
 #To be able to call a function and have that value saved to a variable, we need to return a value:
 def add_two(num):
     return num + 2
-    
+                        
 result = add_two(2)
 print(result)
+
+
+
